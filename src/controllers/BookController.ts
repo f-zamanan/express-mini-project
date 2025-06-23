91ad82e9b019ea65528f944306e78e5300387e5f
f-zamanan <f.n.zamanan@gmail.com>
2025-06-23 20:36:14 +0300

    tried to solve the filtering icebox, no errors but couldn't verify in postman

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -2,13 +2,19 @@ import { Request, Response, NextFunction } from "express";
 import Book from "../models/Book";
 import Author from "../models/Author";
 import Category from "../models/Category";
+import { Query } from "mongoose";
 
 // implementing all CRUD methods for AuthorController
 
 // get all books
 const getAllBooks = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const allBook = await Book.find()
+    const { authID, catID, partialTitle } = req.params;
+    const allBook = await Book.find({
+      author: authID,
+      category: catID,
+      title: partialTitle,
+    }) // filtering the get req localhost:8000/books?author=68596fe7e645c72792846659&category=68596f2ae645c7279284664a&title=Harry
       .populate({ path: "author", select: "name-_id" })
       .populate({ path: "category", select: "name-_id" });
     // ("author category");
